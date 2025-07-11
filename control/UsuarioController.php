78e00637b41dc5fdc41caa47c91c9511a89d1253
didi291 <ethel6814@gmail.com>
2025-07-11 12:50:42 -0500

    iiiii

--- control/UsuarioController.php
+++ control/UsuarioController.php
@@ -6,51 +6,59 @@ $objPersona = new UsuarioModel();
 $tipo = $_GET['tipo'];
 
 if ($tipo == 'registrar') {
-   // print_r ($_POST);
-   $nro_identidad = $_POST['nro_identidad'];
-   $razon_social = $_POST['razon_social'];
-   $telefono = $_POST['telefono'];
-   $correo = $_POST['correo'];
-   $departamento = $_POST['departamento'];
-   $provincia = $_POST['provincia'];
-   $distrito = $_POST['distrito'];
-   $cod_postal = $_POST['cod_postal'];
-   $direccion = $_POST['direccion'];
-   $rol = $_POST['rol'];
-   //ENCRIPTANDO DNI nro_identidad PARA UTILIZARLO COMO CONTRASEÑA
-   $password = password_hash($nro_identidad, PASSWORD_DEFAULT);
+    // print_r ($_POST);
+    $nro_identidad = $_POST['nro_identidad'];
+    $razon_social = $_POST['razon_social'];
+    $telefono = $_POST['telefono'];
+    $correo = $_POST['correo'];
+    $departamento = $_POST['departamento'];
+    $provincia = $_POST['provincia'];
+    $distrito = $_POST['distrito'];
+    $cod_postal = $_POST['cod_postal'];
+    $direccion = $_POST['direccion'];
+    $rol = $_POST['rol'];
+    //ENCRIPTANDO DNI nro_identidad PARA UTILIZARLO COMO CONTRASEÑA
+    $password = password_hash($nro_identidad, PASSWORD_DEFAULT);
 
-   if ($nro_identidad == "" || $razon_social == "" || $telefono == "" || $correo == "" || $departamento == "" || $provincia == "" || $distrito == "" || $cod_postal == "" || $direccion == "" || $rol == "") {
-      $arrResponse = array('status' => false, 'msg' => 'Error, campos vacios');
-   } else {
-      //validacion si existe la misma persona con el mismo dni
-      $existePersona = $objPersona->existePersona($nro_identidad);
-      if ($existePersona > 0) {
-         $arrResponse = array('status' => false, 'msg' => 'Error, nro de documento ya existe');
-      } else {
+    if ($nro_identidad == "" || $razon_social == "" || $telefono == "" || $correo == "" || $departamento == "" || $provincia == "" || $distrito == "" || $cod_postal == "" || $direccion == "" || $rol == "") {
+        $arrResponse = array('status' => false, 'msg' => 'Error, campos vacios');
+    } else {
+        //validacion si existe la misma persona con el mismo dni
+        $existePersona = $objPersona->existePersona($nro_identidad);
+        if (!$existePersona ) {
+            $arrResponse = array('status' => false, 'msg' => 'Error, nro de documento ya existe');
+        } else {
 
-         $respuesta = $objPersona->registrar($nro_identidad, $razon_social, $telefono, $correo, $departamento, $provincia, $distrito, $cod_postal, $direccion, $rol, $password);
-         if ($respuesta) {
-            $arrResponse = array('status' => true, 'msg' => 'Registrado Correctamente');
-         } else {
-            $arrResponse = array('status' => false, 'msg' => 'Error, fallo en registro');
-         }
-      }
-   }
-   echo json_encode($arrResponse);
+            $respuesta = $objPersona->registrar($nro_identidad, $razon_social, $telefono, $correo, $departamento, $provincia, $distrito, $cod_postal, $direccion, $rol, $password);
+            if ($respuesta) {
+                $arrResponse = array('status' => true, 'msg' => 'Registrado Correctamente');
+            } else {
+                $arrResponse = array('status' => false, 'msg' => 'Error, fallo en registro');
+            }
+        }
+    }
+    echo json_encode($arrResponse);
 }
 if ($tipo == "iniciar_sesion") {
     $nro_identidad = $_POST['username'];
     $password = $_POST['password'];
     if ($nro_identidad == "" || $password == "") {
         $respuesta = array('status' => false, 'msg' => 'Error, campos vacios');
-    }else{
+    } else {
         $existePersona = $objPersona->existePersona($nro_identidad);
         if ($existePersona == 0) {
             $respuesta = array('status' => false, 'msg' => 'Error, el usuario no existe');
-        }else{
+        } else {
             $persona = $objPersona->buscarPersonaPorNroIdentidad($nro_identidad);
-            print_r($persona);
+            if (password_verify($password, $persona->password)) {
+                session_start();
+                $_SESSION['ventas_id'] = $persona->id;
+                $_SESSION['ventas_usuario'] = $persona->razon_social;
+                $respuesta = array('status' => true, 'msg' => 'Bienvenido al sistema');
+            } else {
+                $respuesta = array('status' => false, 'msg' => 'Error, contraseña incorrecta');
+            }
         }
     }
+    echo json_encode($respuesta);
 }
